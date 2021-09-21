12111fb043e70c3c5b5e47fefbe0a8f5cfc21464
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-09-21 23:36:28 +0530

    [Amit] Added: a new file of day 3 problems.

---
+++ src/com/amitsisodiya/wagebuilder/EmployeeWageBuilderUC1.java
@@ -0,0 +1,17 @@
+package com.amitsisodiya.wagebuilder;
+
+public class EmployeeWageBuilderUC1 {
+
+	public static void main(String[] args) {
+		int empIsPresent = 1;
+		int  empCheck = (int)Math.floor(Math.random()*10 %2);
+		if (empCheck == empIsPresent) {
+			System.out.println("Employee is present.");
+		}
+		else 
+			System.out.println("Employee is absent.");
+			
+		
+	}
+
+}
